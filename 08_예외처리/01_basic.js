c70391b3968b6a0cfe2c0b49d83d79faef1c0bfe
Yeongil Kim <skyksdc01@gmail.com>
2020-09-24 10:00:24 +0900

    예외 처리(Lecture_0924)

---
+++ 08_예외처리/01_basic.js
@@ -0,0 +1,7 @@
+try {
+    error.error.error();                // try {error가 의심되는 문장}
+} catch (e) {
+    console.log(e.name);
+    console.log(e.message);
+}
+console.log('프로그램 정상 종료');
---
+++ 08_예외처리/02_예외발생.js
@@ -0,0 +1,12 @@
+// 함수 선언
+function callThreeTimes(callback) {
+    for (let i=0; i<3; i++) {
+        callback();
+    }
+}
+// 정상 실행
+callThreeTimes(function() {
+    console.log('호출!');
+});
+//예외 발생
+callThreeTimes();
