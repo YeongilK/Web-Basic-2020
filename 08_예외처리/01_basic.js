2681385bdecf4848fb332dac8a9edeedcaf49cca
Yeongil Kim <skyksdc01@gmail.com>
2020-09-24 10:06:37 +0900

    delete

--- 08_예외처리/01_basic.js
+++
@@ -1,8 +0,0 @@
-try {
-    error.error.error();                // try {error가 의심되는 문장}
-} catch (e) {
-    console.log(e.name);
-    console.log(e.message);
-}
-
-console.log('프로그램 정상 종료');
--- 08_예외처리/05_고급예외처리.js
+++
@@ -1,9 +0,0 @@
-let size = -10;
-
-try {
-    let array = new Array(size);
-} catch (e) {
-    console.log(e.name);
-    console.log(e.message);
-    console.log('양의 정수를 입력하세요!!');
-}
